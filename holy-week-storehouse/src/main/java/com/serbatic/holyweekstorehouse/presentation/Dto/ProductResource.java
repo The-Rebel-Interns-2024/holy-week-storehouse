a22d754004c26e65de5914e8e69f1594d6b56fe9
lgarciaSB <lgarcia@serbatic.es>
2024-03-26 14:52:04 +0100

    refactor: Modify name of class and parameter name

--- holy-week-storehouse/src/main/java/com/serbatic/holyweekstorehouse/presentation/Dto/ProductResource.java
+++ holy-week-storehouse/src/main/java/com/serbatic/holyweekstorehouse/presentation/Dto/ProductResource.java
@@ -1,23 +1,23 @@
 package com.serbatic.holyweekstorehouse.presentation.Dto;
 
-public class ProductRequest {
-    String name;
+public class ProductResource {
+    String code;
     Integer quantity;
 
-    public ProductRequest() {
+    public ProductResource() {
     }
 
-    public ProductRequest(String name, Integer quantity) {
-        this.name = name;
+    public ProductResource(String code, Integer quantity) {
+        this.code = code;
         this.quantity = quantity;
     }
 
-    public String getName() {
-        return name;
+    public String getCode() {
+        return code;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCode(String code) {
+        this.code = code;
     }
 
     public Integer getQuantity() {
